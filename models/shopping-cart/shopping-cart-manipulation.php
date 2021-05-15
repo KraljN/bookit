033c5c5dd83bd3338a01c20228b7d611e37ccb6c
KraljN <nikolakralj9@gmail.com>
2021-05-15 15:48:33 +0200

    Back-end for adding products in shopping cart

--- models/shopping-cart/shopping-cart-manipulation.php
+++ models/shopping-cart/shopping-cart-manipulation.php
@@ -1,43 +1,42 @@
 <?php 
 session_start();
-// unset($_SESSION["shoppingCart"]);
-// unset($_SESSION["shopingCart"]);
+unset($_SESSION["shoppingCart"]);
 
-if(isset($_POST["action"])){
-    if($_POST["action"] == "add"){
-        if(!isset($_SESSION["shoppingCart"])){
-            $_SESSION["shoppingCart"] = [];
-            $cartProductsIds=[];
-        }
-        else{
-            $cartProductsIds = array_keys($_SESSION["shoppingCart"]);
-        }
-        $productId = $_POST["id"];
-        if(in_array($productId, $cartProductsIds)){
-            $_SESSION["shoppingCart"][$productId]["productQuantity"]++;
-        }
-        else{
-            $_SESSION["shoppingCart"][$productId]["productQuantity"] = 1;
-        }
-    }
-    if($_POST["action"]=="remove"){
+// if(isset($_POST["action"])){
+//     if($_POST["action"] == "add"){
+//         if(!isset($_SESSION["shoppingCart"])){
+//             $_SESSION["shoppingCart"] = [];
+//             $cartProductsIds=[];
+//         }
+//         else{
+//             $cartProductsIds = array_keys($_SESSION["shoppingCart"]);
+//         }
+//         $productId = $_POST["id"];
+//         if(in_array($productId, $cartProductsIds)){
+//             $_SESSION["shoppingCart"][$productId]["productQuantity"]++;
+//         }
+//         else{
+//             $_SESSION["shoppingCart"][$productId]["productQuantity"] = 1;
+//         }
+//     }
+//     if($_POST["action"]=="remove"){
         
-        $id = $_POST["id"];
-        foreach($_SESSION["shoppingCart"] as $i => $product){
-            if($i == $id){
-                unset($_SESSION["shoppingCart"][$i]);
-            }
-        }
-    }
-    if($_POST["action"] == "changeQuantity"){
-        $quantity = $_POST["quantity"];
-        foreach($_SESSION["shoppingCart"] as $i => $product){
-            if($i == $_POST["id"]){
-                $_SESSION["shoppingCart"][$i]["productQuantity"] = $quantity;
-            }
-        }
-    }
-}
-else{
-    header("Location: ../index.php");
-}
+//         $id = $_POST["id"];
+//         foreach($_SESSION["shoppingCart"] as $i => $product){
+//             if($i == $id){
+//                 unset($_SESSION["shoppingCart"][$i]);
+//             }
+//         }
+//     }
+//     if($_POST["action"] == "changeQuantity"){
+//         $quantity = $_POST["quantity"];
+//         foreach($_SESSION["shoppingCart"] as $i => $product){
+//             if($i == $_POST["id"]){
+//                 $_SESSION["shoppingCart"][$i]["productQuantity"] = $quantity;
+//             }
+//         }
+//     }
+// }
+// else{
+//     header("Location: ../index.php");
+// }
--- views/shopping-cart.php
+++ views/shopping-cart.php
@@ -1,3 +1,4 @@
+<?php require_once "config/connection.php"?>
 <div class="container">
 <div class="card shopping-cart">
          <div class="card-header bg-dark text-light">
@@ -10,68 +11,76 @@
                  <?php 
 
 
-                $query = "SELECT b.book_id AS id, bi.path, bi.alt, p.value AS price, b.title
-                            FROM book_images bi  
-                            INNER JOIN books b ON bi.book_id = b.book_id 
-                            INNER JOIN books_prices bp ON b.book_id = bp.book_id
-                            INNER JOIN prices p ON (SELECT price_id 
-                                                    FROM books_prices bp
-                                                    WHERE bp.book_id = b.book_id
-                                                    ORDER BY date_become_effective DESC
-                                                    LIMIT 1)  = p.price_id";
-
+                if(isset($_SESSION["shoppingCart"])):
+                    $cartProductsIds = array_keys($_SESSION["shoppingCart"]);
+                    $ids = join(",", $cartProductsIds);
+                    $query = "SELECT b.book_id AS id, bi.path, bi.alt, p.value AS price, b.title
+                    FROM book_images bi  
+                    INNER JOIN books b ON bi.book_id = b.book_id 
+                    INNER JOIN books_prices bp ON b.book_id = bp.book_id
+                    INNER JOIN prices p ON (SELECT price_id 
+                                            FROM books_prices bp
+                                            WHERE bp.book_id = b.book_id
+                                            ORDER BY date_become_effective DESC
+                                            LIMIT 1)  = p.price_id
+                    WHERE b.book_id IN ($ids)";
+                    $results  = $db -> query($query)->fetchAll();
+                    
                 
-
-                  if(isset($_SESSION["shoppingCart"])):
-                        foreach($_SESSION["shoppingCart"] as $i => $quantity):
+                        foreach($results as $bookInCart):
                  ?>
+
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-2 text-center mb-2 mb-sm-0">
+                                <img class="img-responsive" src="<?= IMG_PATH  . "thumb-" . $bookInCart->path  ?>" alt="<?= $bookInCart->alt ?>"/>
+                        </div>
+                        <div class="col-12 text-sm-center col-sm-12 text-center text-md-left col-md-4 d-flex flex-column justify-content-center">
+                            <h4 class="product-name"><strong><?= $bookInCart->title ?></strong></h4>
+                        </div>
+                        <div class="col-12 col-sm-12 text-sm-center col-md-6 text-md-right d-flex align-items-center justify-content-center flex-column flex-sm-row row">
+                            <div class="col-6 text-center mb-2 mb-sm-0 col-sm-3 col-md-4 text-md-right pt-2">
+                                <h6><strong><?= $bookInCart->price ?> &euro;</strong></h6>
+                            </div>
+                            <div class="col-6 col-sm-4 mb-2 mb-sm-0 d-flex justify-content-center">
+                                <div class="quantity">
+                                    <input type="button" value="+" class="plus">
+                                    <input type="number" step="1" max="99" min="1" value="<?= $_SESSION["shoppingCart"][$bookInCart->id]["productQuantity"] ?>" title="Qty" class="qty"
+                                            size="4">
+                                    <input type="button" value="-" class="minus">
+                                </div>
+                            </div>
+                            <div class="col-5 col-sm-3 text-center text-sm-left">
+                                <button type="button" class="btn red-button btn-xs">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+                 </div>
+                 <hr>
 
                  <?php
                     endforeach;
                     else: 
                  ?>
+                    <div id="emptyCart" class="d-flex justify-content-center align-items-center">
+                        <h2>Your shopping cart is currently empty.</h2>
+                    </div>
                  <?php endif ?>
-                 <div class="row">
-                     <div class="col-12 col-sm-12 col-md-2 text-center mb-2 mb-sm-0">
-                             <img class="img-responsive" src="https://place-hold.it/80x120" alt="prewiew"/>
-                     </div>
-                     <div class="col-12 text-sm-center col-sm-12 text-center text-md-left col-md-4 d-flex flex-column justify-content-center">
-                         <h4 class="product-name"><strong>Product Name</strong></h4>
-                         <h4>
-                             <small>Product description</small>
-                         </h4>
-                     </div>
-                     <div class="col-12 col-sm-12 text-sm-center col-md-6 text-md-right d-flex align-items-center justify-content-center flex-column flex-sm-row row">
-                         <div class="col-6 text-center mb-2 mb-sm-0 col-sm-3 col-md-4 text-md-right pt-2">
-                             <h6><strong>25.00 &euro;</strong></h6>
-                         </div>
-                         <div class="col-6 col-sm-4 mb-2 mb-sm-0 d-flex justify-content-center">
-                             <div class="quantity">
-                                 <input type="button" value="+" class="plus">
-                                 <input type="number" step="1" max="99" min="1" value="1" title="Qty" class="qty"
-                                        size="4">
-                                 <input type="button" value="-" class="minus">
-                             </div>
-                         </div>
-                         <div class="col-5 col-sm-3 text-center text-sm-left">
-                             <button type="button" class="btn red-button btn-xs">
-                                 <i class="fa fa-trash" aria-hidden="true"></i>
-                             </button>
-                         </div>
-                     </div>
-                 </div>
-                 <hr>
-                 <!-- END PRODUCT -->
          </div>
          <div class="card-footer">
-             <div class="coupon col-md-5 col-sm-5 no-padding-left pull-left">
-             </div>
+            <?php if(isset($_SESSION["shoppingCart"])): ?>
              <div class="float-right m-3">
-                 <a href="" class="btn">Purchase</a>
+                 <button class="btn" id="purchase">Purhcase</button>
                  <div class="pull-right m-2">
                      Total price: <b>50.00€</b>
                  </div>
              </div>
+             <?php else: ?>
+                <div class="float-right m-3">
+                    <a href="index.php?page=shop" class="btn">See Offer</a>
+                 </div>
+             </div>
+            <?php endif ?>
          </div>
      </div>
 </div>
